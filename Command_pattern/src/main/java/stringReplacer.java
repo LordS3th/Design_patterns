4cb466321768ec866ac5667e9872cef2efe523a9
LordS3th <84471154+LordS3th@users.noreply.github.com>
2021-12-03 17:52:41 +0000

    FInished String replacer

--- Command_pattern/src/main/java/stringReplacer.java
+++ Command_pattern/src/main/java/stringReplacer.java
@@ -1,19 +1,13 @@
 class stringReplacer implements StringTransformer {
     char to_replace;
-    char replaced;
-    public stringReplacer(char to_replace, char replaced){
+    char replacer;
+    public stringReplacer(char to_replace, char replacer){
         this.to_replace=to_replace;
-        this.replaced= replaced;
+        this.replacer= replacer;
     }
     public void execute(StringDrink drink) {
         String text = drink.getText();
-        String res= "";
-        for (int i =0; i < text.length();i++){
-            if (text.charAt(i) == to_replace){
-                res+=replaced;
-            }
-            else res+=text.charAt(i);
-        }
-        drink.setText(res);
+        text=text.replace(to_replace,replacer);
+        drink.setText(text);
     }
 }
